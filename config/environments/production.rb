13bc433d3a35ae0abc2a036ead4eb2f2e2387b96
GupiSakaguch <gupisakaguchi@gmail.com>
2018-01-30 11:00:50 +0900

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,9 +44,6 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
@@ -88,4 +85,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
 end
