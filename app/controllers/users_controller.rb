1f858de74d83b5a98bb4f8ad0ccd65f2ec2c3156
GupiSakaguch <gupisakaguchi@gmail.com>
2018-02-01 17:35:08 +0900

    Add friendly-forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,13 +44,14 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in"
       redirect_to login_url
     end
   end
 
   def correct_user
-    @user = User.find(paramas[:id])
+    set_user
     redirect_to(root_url) unless current_user?(@user)
   end
 
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -43,4 +43,15 @@ module SessionHelper
     user == current_user
   end
 
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || url)
+    sessein.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
