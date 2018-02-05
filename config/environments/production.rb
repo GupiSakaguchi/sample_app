011478dd649224c3916745d17e33bcd7910eaa17
GupiSakaguch <gupisakaguchi@gmail.com>
2018-02-05 17:10:39 +0900

    Change config.assets.compile to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,10 @@ Rails.application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
